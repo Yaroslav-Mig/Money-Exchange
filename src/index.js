fb47cd005b22084104ca01b281c0f18a762eda07
Yaroslav-Mig <migyaroslav@gmail.com>
2018-09-30 21:30:41 +0300

    upload

--- src/index.js
+++ src/index.js
@@ -1,5 +1,63 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency > 10000) {
+        return error = {
+            error: "You are rich, my friend! We don't have so much coins for exchange"
+        };
+    } else if (currency > 0) {
+        const arr = [50, 25, 10, 5, 1];
+        const lenght = arr.length;
+        let count;
+        let countFirst = '';
+        let countSecond = '';
+
+        for (let i = 0; i < lenght; i++) {
+            let flag = true;
+            for (let j = i; arr[j] <= currency; j++) {
+                changeCoins(currency, j);
+                flag = false;
+                break;
+            }
+            if (flag) {
+                countFirst += 0 + ' ';
+                continue;
+            }
+            break
+        }
+
+        function changeCoins(currency, index) {
+            const coins = arr;
+            for (let i = index; i < lenght; i++) {
+                let coin = coins[i];
+                let quantity = Math.floor(currency / coin);
+                let rest = currency % coin;
+                currency = rest;
+                countSecond += quantity + ' ';
+            }
+            return countSecond;
+        }
+
+        count = countFirst + countSecond;
+        let values = count.split(' ');
+        values = values.map(value => Number(value));
+
+        let store = {
+            'H': 0,
+            'Q': 0,
+            'D': 0,
+            'N': 0,
+            'P': 0
+        };
+        let i = 0;
+        for (let key in store) {
+            store[key] = values[i]
+            i++;
+            if (store[key] === 0) {
+                delete store[key];
+            }
+        }
+        return store;
+    } else {
+        return error = {};
+    }
 }
